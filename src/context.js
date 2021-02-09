60052c24f1a7c5ca8209b27dc586c97ffccea85f
mia <cocoball200@gmail.com>
2021-02-09 22:55:11 +0900

    Add: add to cart, important! should repeat!

--- src/context.js
+++ src/context.js
@@ -10,6 +10,7 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
+    cart: [],
   }
   componentDidMount() {
     this.setProduct();
@@ -22,13 +23,35 @@ class ProductProvider extends Component {
     })
     this.setState(() => {
       return { products: tempProduct };
+    });
+  };
+
+  getItem = (id) => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
+  }
+
+  handleDetail = (id) => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product }
     })
+
   }
-  handleDetail = () => {
-    console.log('hello from detail')
-  }
-  addToCart = () => {
-    console.log('hello from add to cart')
+  addToCart = (id) => {
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+
+    this.setState(() => {
+      return { products: tempProducts, cart: [...this.state.cart, product] };
+    }, () => { console.log(this.state) })
+
+
   }
 
   render() {
